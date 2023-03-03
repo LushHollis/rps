aed5c42ae682b9fa13821d6598a4690808c2685c
Lush <lushhollis@gmail.com>
2023-03-02 19:21:30 -0500

    Create playGame()
    - playGame() was created and linked to button onClick
    - console.log was changed to results.innerHTML

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
 const random=(min,max)=>{return Math.floor(Math.random() * (max - min + 1) + min);}
-let userinput="rock";
-let compinput;
-switch (random(1,3)){
-    case 1:
-        compinput="rock";
-        break;
-    case 2:
-        compinput="paper";
-        break;
-    case 3:
-        compinput="scissors";
-        break;
-    default:
-        console.warn("default in switch");
-        compinput="rock";
-}
+let results=document.getElementById('results');
 
-if (userinput=="rock"){
-    if (compinput=="rock"){
-        console.log("tie");
+function playGame(userinput){
+    let compinput;
+    switch (random(1,3)){
+        case 1:
+            compinput="rock";
+            break;
+        case 2:
+            compinput="paper";
+            break;
+        case 3:
+            compinput="scissors";
+            break;
+        default:
+            console.warn("default in switch");
+            compinput="rock";
     }
-    else if (compinput=="paper"){
-        console.log("comp wins");
+
+    if (userinput=="rock"){
+        if (compinput=="rock"){
+            results.innerHTML="It's a tie!";
+        }
+        else if (compinput=="paper"){
+            results.innerHTML="The computer wins...";
+        }
+        else{
+            results.innerHTML="You win!";
+        }
+    }
+    else if (userinput=="paper"){
+        if (compinput=="rock"){
+            results.innerHTML="You win!";
+        }
+        else if (compinput=="paper"){
+            results.innerHTML="It's a tie!";
+        }
+        else{
+            results.innerHTML="The computer wins...";
+        }
     }
     else{
-        console.log("user wins");
-    }
-}
-else if (userinput=="paper"){
-    if (compinput=="rock"){
-        console.log("user wins");
-    }
-    else if (compinput=="paper"){
-        console.log("tie");
-    }
-    else{
-        console.log("comp wins");
-    }
-}
-else{
-    if (compinput=="rock"){
-        console.log("comp wins");
-    }
-    else if (compinput=="paper"){
-        console.log("user wins");
-    }
-    else{
-        console.log("tie");
+        if (compinput=="rock"){
+            results.innerHTML="The computer wins...";
+        }
+        else if (compinput=="paper"){
+            results.innerHTML="You win!";
+        }
+        else{
+            results.innerHTML="It's a tie!";
+        }
     }
 }
